d30aef83032d04ca00a038a74c4fb13de742714c
edi-sipka@gmail.com <edisipka@gmail.com>
2022-09-21 12:45:31 +0200

    Add useSelector and useDispatch

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,12 +1,45 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
+  const dispatch = useDispatch();
+  const [inputState, setState] = useState({ title: '', author: '' });
+  const changeState = (e) => {
+    e.preventDefault();
+    setState({ ...inputState, [e.target.name]: e.target.value });
+  };
+
+  const bookState = (e) => {
+    e.preventDefault();
+    if (!inputState.title.trim() || !inputState.author.trim()) return;
+    const book = {
+      id: uuidv4(),
+      title: inputState.title,
+      author: inputState.author,
+    };
+    dispatch(addBook(book));
+    setState({ title: '', author: '' });
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="title" />
-        <input type="text" placeholder="author" />
+      <form onSubmit={bookState}>
+        <input
+          type="text"
+          placeholder="title"
+          value={inputState.title}
+          onChange={changeState}
+          name="title"
+        />
+        <input
+          type="text"
+          placeholder="author"
+          value={inputState.author}
+          onChange={changeState}
+          name="author"
+        />
         <button type="submit">Add book</button>
       </form>
     </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable */
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 const Categories = () => {
+  const sign = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const statusCheck = () => {
+    dispatch(checkStatus());
+  };
   return (
     <div>
-      <button className="{styles.btn}" type="submit">
+      <button className="{styles.btn}" type="button" onClick={statusCheck}>
         Check status
       </button>
+      <h4>{sign}</h4>
     </div>
   );
 };
